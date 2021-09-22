5c5d33668d17acd69e19dfb8932350b5e5194a1c
Jonathan Rod <jonyrm91@gmail.com>
2021-09-22 10:57:33 +0200

    Fetch to the Giphy API and tested the result from the endpoint on browser

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
   // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
-  const [categories, setCategories] = useState([
-    'One Punch',
-    'Samurai X',
-    'Dragon Ball',
-  ]);
+  const [categories, setCategories] = useState(['Dragon Ball']);
 
   // Add new category to the array of categories
   // const handleAdd = (category) => {
@@ -22,9 +19,11 @@ export const GifExpertApp = () => {
       <hr />
 
       <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
+        {categories.map((category) => (
+          <GifGrid 
+          key={category} 
+          category={category} />
+        ))}
       </ol>
     </>
   );
