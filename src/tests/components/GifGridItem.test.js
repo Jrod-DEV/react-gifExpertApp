1a7bc67258e8854bea7d4f9c5b5a8b7675e0bb52
Jonathan Rod <jonyrm91@gmail.com>
2021-09-23 13:28:19 +0200

    Test of img props

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -13,11 +13,32 @@ import { GifGridItem } from '../../components/GifGridItem';
  */
 
 describe('Pruebas en <GifGridItem />>', () => {
-    const title = 'Un titulo';
-    const url = 'https://example.com/algo.jpg';
+  const title = 'Un titulo';
+  const url = 'https://example.com/algo.jpg';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
   test('debe de mostrar el componente correctamente', () => {
-    const wrapper = shallow(<GifGridItem  title={title} url={url}/>);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('debe tener un <p> con el title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('debe de tener la imagen igual al url y alt de los props', () => {
+    const img = wrapper.find('img');
+    // console.log(img.props().src);
+    expect(img.prop('src')).toBe(url);
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test('debe tener animate__fadeIn', () => {
+    const div = wrapper.find('div');
+    const className = div.prop('className');
+    const cssAnimation = 'animate__fadeIn';
+
+
+    expect(className.includes(cssAnimation)).toBe(true);
   });
 });
